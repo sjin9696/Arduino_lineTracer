791ff417c6ea4f783d4c188f635996515f5cd0cd
sjin9696 <sjin9696@gmail.com>
2020-10-29 09:57:56 +0900

    최초 커밋(모터+적외선센서) 구현중
    2020-10-29 최초 커밋(모터+적외선센서) 구현중

---
+++ libTest.h
@@ -0,0 +1,11 @@
+#ifndef libTest_h
+#define libTest_h
+
+String sensor_print(int num,int *val);
+bool Line(int n);
+String Result_Print(int *A);
+void Init(int *A);
+void Init_Setup();
+String Sensor_Loop();
+
+#endif
