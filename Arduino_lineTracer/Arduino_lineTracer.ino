e47d6f8719ec544e8cf755633f0522e93d7679e8
sjin9696 <sjin9696@gmail.com>
2020-11-06 20:13:32 +0900

    MP3기능통합구현완료

--- Arduino_lineTracer/Arduino_lineTracer.ino
+++ Arduino_lineTracer/Arduino_lineTracer.ino
@@ -2,6 +2,8 @@
 #include "Flasher.h"
 #include "choumpa.h"
 #include "RGBColorSensor.h"
+#include "MP3.h"
+
 Flasher interver1(12,1000,5000);
 
 void setup() {
@@ -10,10 +12,13 @@ void setup() {
   Init_Setup();
   Ultrasonicsetup();
   RGBsensor_setup() ;
+  MP3setup(30);
+  
 }
 void loop() {
  Car_loop(Sensor_Loop());
  /* 초음파 센서로 비상정지 기능 적용하기 */
  Serial.println(RGBsensor_loop());
  Serial.println(UltrasonicSensor());
+ MP3loop(3);
 }
